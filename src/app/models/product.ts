43a1de2dc01032ae35cd7a767349aa5c9a98313d
Jlafratta <julian.lafratta@gmail.com>
2020-11-05 17:38:01 -0300

    models mapped

---
+++ src/app/models/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+
+    productId: number;
+    productTypeId: number;
+    name: string;
+    description: string;
+    price: number;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+
+    userId: number;
+    userTypeId: number;
+    name: string;
+    email: string;
+    password: string;
+}
